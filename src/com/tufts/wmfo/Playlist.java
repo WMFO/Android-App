fe6d7babf909ba5a5618ecffbe72d24508131907
Ross Schlaikjer <ross@schlaikjer.net>
2012-12-05 01:16:52 -0500

    Null-safed playlist

--- src/com/tufts/wmfo/Playlist.java
+++ src/com/tufts/wmfo/Playlist.java
@@ -17,41 +17,43 @@ import android.util.Log;
 
 public class Playlist {
 	//http://spinitron.com/radio/rss.php?station=wmfo
-	
+
 	ArrayList<SongInfo> Songs;
-	
+
 	public Playlist(String RSS){
 		Songs = new ArrayList<SongInfo>();
-		Document playlistXML = getDomElement(RSS);
-		NodeList entries = playlistXML.getElementsByTagName("item");
-		for (int i=0; i < entries.getLength(); i++){
-			Songs.add(new SongInfo(entries.item(i)));
+		if (RSS != null && !RSS.equals("")){
+			Document playlistXML = getDomElement(RSS);
+			NodeList entries = playlistXML.getElementsByTagName("item");
+			for (int i=0; i < entries.getLength(); i++){
+				Songs.add(new SongInfo(entries.item(i)));
+			}
 		}
 	}
 
 	public Document getDomElement(String xml){
-        Document doc = null;
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        try {
- 
-            DocumentBuilder db = dbf.newDocumentBuilder();
- 
-            InputSource is = new InputSource();
-                is.setCharacterStream(new StringReader(xml));
-                doc = db.parse(is); 
- 
-            } catch (ParserConfigurationException e) {
-                Log.e("Error: ", e.getMessage());
-                return null;
-            } catch (SAXException e) {
-                Log.e("Error: ", e.getMessage());
-                return null;
-            } catch (IOException e) {
-                Log.e("Error: ", e.getMessage());
-                return null;
-            }
-                // return DOM
-            return doc;
-    }
-	
+		Document doc = null;
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		try {
+
+			DocumentBuilder db = dbf.newDocumentBuilder();
+
+			InputSource is = new InputSource();
+			is.setCharacterStream(new StringReader(xml));
+			doc = db.parse(is); 
+
+		} catch (ParserConfigurationException e) {
+			Log.e("Error: ", e.getMessage());
+			return null;
+		} catch (SAXException e) {
+			Log.e("Error: ", e.getMessage());
+			return null;
+		} catch (IOException e) {
+			Log.e("Error: ", e.getMessage());
+			return null;
+		}
+		// return DOM
+		return doc;
+	}
+
 }
